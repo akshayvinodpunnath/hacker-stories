6a177a9aef3a923d7f9f8f959224155d898984f6
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-21 23:43:58 +0530

    App.js Modification - Welcome variable removed;h1 tag modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-};
-
-
 function App(){
   return(
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}
+        Hey React
       </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
