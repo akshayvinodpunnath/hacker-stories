df6059397b02b4c4db74b720e6cd7acaa86bdb26
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-22 23:11:36 +0530

    remove block body from List and App components in App.js

--- src/App.js
+++ src/App.js
@@ -19,23 +19,21 @@ const list = [
   },
 ];
 
-const List = () => {
-  return list.map(function(item) {
-    return (
-      <div key={item.objectID}>
+const List = () => (
+  list.map(item => (
+    <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
         <span> {item.author}</span>
         <span> {item.num_comments}</span>
         <span> {item.points}</span>
-      </div>
+      </div>  
     )
-  })
-}
+  )
+)
 
-const App = () => {
-  return(
+const App = () => (
     <div>
       <h1>
         My Hacker Stories
@@ -47,7 +45,6 @@ const App = () => {
       
       <List />
     </div>
-  )
-}
+);
 
 export default App;
