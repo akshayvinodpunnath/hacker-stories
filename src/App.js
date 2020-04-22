10a12ae0cf22c126191173876490a69ff793f70b
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-22 22:17:30 +0530

    List iteration modified to include additional properties in App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,16 @@ function App(){
 
       <hr />
       {list.map(function(item) {
-        return <div key={item.objectID}>{item.title}</div>
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </div>
+        )
       })}
     </div>
   )
