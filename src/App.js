1796b9b0230d902083ef49dce524623b3bc40a02
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-24 00:25:05 +0530

    props modified to {search, onSearch} in Search Component - App.js

--- src/App.js
+++ src/App.js
@@ -62,13 +62,11 @@ const App = () => {
     );
 };
 
-const Search = props => {
-  const {search, onSearch} = props
-  return (
+const Search = ({search, onSearch}) => (
     <div>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" value={search} onChange={onSearch}/>
     </div>
-  )
-}
+);
+
 export default App;
