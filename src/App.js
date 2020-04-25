03c77bf52cca4d594dc283eaa4646ebaa00ce17b
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-25 23:39:06 +0530

    spread operator used in List component for passing all object's key/value pair

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ const List = ({list}) => (
   list.map(item => (
   <Item 
     key={item.objectID} 
-    title={item.title}
-    url={item.url}
-    author={item.author}
-    num_comments={item.num_comments}
-    points={item.points} 
+    {...item}
     /> 
     )
   )
