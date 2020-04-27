0dc704e8ac6ab7b46a73c7556439c1228a7a6c21
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-04-27 23:12:46 +0530

    Updates to InputWithLabel Component - 1) create a ref with useRef hook. 2) ref passed to the input field's JSX-reserved ref attribute 3) opt into React's Lifecycle with React's useEffect 4) Execute focus programmatically

--- src/App.js
+++ src/App.js
@@ -71,19 +71,31 @@ const InputWithLabel = ({
   onInputChange,
   isFocused,
   children
-  }) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input
-      id={id}
-      type={type}
-      value={value}
-      autoFocus={isFocused}
-      onChange={onInputChange}
-    />
-  </>
-);
+  }) => {
+  
+    const inputRef = React.useRef();
+
+    React.useEffect(() => {
+      if(isFocused && inputRef.current) {
+        inputRef.current.focus()
+      }
+    },[isFocused]);
+
+    return (
+      <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+        <input
+          ref={inputRef}
+          id={id}
+          type={type}
+          value={value}
+          autoFocus={isFocused}
+          onChange={onInputChange}
+        />
+      </>
+    )
+};
 
 const List = ({ list }) =>
   list.map(item => <Item key={item.objectID} item={item} />);
